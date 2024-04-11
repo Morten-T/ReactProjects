47461b8155a40f67fdef1c42d0bf3e7c96d41178
Morten-T <114079510+Morten-T@users.noreply.github.com>
2024-04-11 23:03:04 +0200

    added resisable boat

--- yarrr-matey/src/App.js
+++ yarrr-matey/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
+// import sailboat from './sailboat.png';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  // get notified whenever the window is resized ( find guiden fra forrige lektion, den med Greg)
+  // window.addEventListener('resize', () => {
+  //   console.log(window.innerWidth);
+  //   console.log(window.innerHeight);
+  // });
+
+  // make event handler for when window is resized
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='yellow-box'>
+        <h1 className='title'>Webpage to calculate hull speed</h1>
+        <img src="/sailboat.jpg" alt='sailboat' style={{ width: '12vw', height: '50vh' }} />
+        {/* Måske lav det om så man bruger en resize listener, og useEffect til at lave om på størrelsen af billedet */}
+      </div>
     </div>
   );
 }
